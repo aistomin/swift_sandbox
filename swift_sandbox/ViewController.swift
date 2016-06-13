5303f6668c25aa83786d3eae4d4dcc8c398af3d9
aistomin <andrej.istomin.ikeen@gmail.com>
2016-06-13 21:24:46 +0200

    app skeleton

---
+++ swift_sandbox/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  swift_sandbox
+//
+//  Created by andrej on 12/06/16.
+//  Copyright © 2016 rising. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
