ba89e188d633927b84d71dd8751052b14dfa0b1d
aistomin <andrej.istomin.ikeen@gmail.com>
2016-07-18 14:25:29 +0200

    display selected meal for editing on table cell selection.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -27,6 +27,12 @@ class ViewController: UIViewController, UITextFieldDelegate,
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
         checkValidMealName()
     }
     
